8ca847357fb95d06108f5dd696c7450efafe3ab5
Ethan Gan <69548912+kobotabby@users.noreply.github.com>
2025-05-12 11:48:10 -0400

    Made styling of articles consistent with homepage and made cards responsive for mobile.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <title>Ethan Gan || About Me</title>
       <h1>About Me</h1>
       <p>
-        I belive in continous iteration and improvement as a former highschool lead trumpet player and current engineering student. From improving Tetris skills, logical reasoning, and mathematical thinking, I learned the most effective way to learn is to sink into the subject with unparalleled curiosity.
+        I believe in continous iteration and improvement as a former highschool lead trumpet player and current engineering student. From improving Tetris skills, logical reasoning, and mathematical thinking, I learned the most effective way to learn is to sink into the subject with unparalleled curiosity.
         I love nature, running, badminton, and teaching, but my current obsession is improving on coding skills to become a better engineer.
         My goal is to be an innovative experience designer of both virtual and physical spaces while staying on top of latest technologies and best practices.  And to be known for experimental ideas and taking risks into new territories where I am lesser experienced in to best improve the quality and value of my work as products to users. Above all, I want to excel in being versatile and open-to-new ideas in the evolving modern workplace.
         <br></br>
