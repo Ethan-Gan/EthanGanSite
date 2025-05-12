a9afb18659d5b1aeffc802fd71afe2874d6f1994
Ethan Gan <69548912+kobotabby@users.noreply.github.com>
2025-05-11 20:04:52 -0400

    removed spaces from resume name

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -8,7 +8,7 @@ function Resume() {
       <title>Ethan Gan || Resume</title>
       {/* <h1>Resume</h1>
       <p>This is where you can find my professional experience and skills.</p> */}
-      <iframe id="myPDF" src="public\content\pdf\R7.0 Ethan X. Gan - Resume.pdf" frameborder="0"></iframe>
+      <iframe id="myPDF" src="public\content\pdf\ethanganresume.pdf" frameborder="0"></iframe>
     </div>
   );
 }
